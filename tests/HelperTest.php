f50959688372c7548408e826a4df974ab1725e87
Eko Junaidi Salam <eko.junaidi.salam@gmail.com>
2022-05-01 15:51:48 +0700

    Fixing testcase for different php version

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -38,13 +38,18 @@ class HelperTest extends TestCase {
 	$this->assertEquals("31 Tahun 10 Bulan 27 Hari",dateDiff((new DateTimeImmutable("1990-06-04", new DateTimeZone("Asia/Jakarta")))->format("Y-m-d"),"2022-05-01"));
 	if(preg_match('/8.1/',phpversion())){
 		$this->assertEquals("01 Bulan",dateDiff("2022-04-01","2022-05-01"));
+		$this->assertEquals("01 Bulan 10 Hari",dateDiff("2022-04-01","2022-05-11"));
+		$this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
+		$this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
+		$this->assertEquals("01 Tahun 02 Bulan",dateDiff("2021-03-01","2022-05-01"));
 	}else{
 		$this->assertEquals("30 Hari",dateDiff("2022-04-01","2022-05-01"));
+		$this->assertEquals("01 Bulan 09 Hari",dateDiff("2022-04-01","2022-05-11"));
+		$this->assertEquals("01 Tahun 01 Hari",dateDiff("2021-05-01","2022-05-03"));
+		$this->assertEquals("11 Hari",dateDiff("2022-05-01","2022-05-13"));
+		$this->assertEquals("01 Tahun 02 Bulan 02 Hari",dateDiff("2021-03-01","2022-05-01"));
+
 	}
-	$this->assertEquals("01 Bulan 10 Hari",dateDiff("2022-04-01","2022-05-11"));
-	$this->assertEquals("01 Tahun 02 Bulan",dateDiff("2021-03-01","2022-05-01"));
-	$this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
-	$this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
 	$this->assertEquals("0 Hari",dateDiff("2022-05-01","2022-05-01"));
     }
 }
