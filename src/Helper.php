b00ae4ab8855ed74a30ec6616bdb34a7e91145ae
Eko Junaidi Salam <eko.junaidi.salam@gmail.com>
2022-05-01 20:09:11 +0700

    Add docs, uuid v3 v4 v5 and http header status

--- src/Helper.php
+++ src/Helper.php
@@ -10,9 +10,14 @@
 if(!function_exists('is_kabisat')){
 // @codeCoverageIgnoreEnd
 
-    function is_kabisat($tahun=null): int {
-        $tahun = intval(!empty($tahun) ? $tahun : date('Y'));
-        return ((($tahun % 4 == 0) && ($tahun % 100 != 0) || ($tahun % 400 == 0))?1:0);
+	/**
+	 * Check year is kabisat or not
+	 * @param $year int Year
+	 * @return int
+	 */
+    function is_kabisat(int $year=0): int {
+        $year = $year > 0 ? $year : date('Y');
+        return ((($year % 4 == 0) && ($year % 100 != 0) || ($year % 400 == 0))?1:0);
     }
 }
 
@@ -20,7 +25,14 @@ if(!function_exists('is_kabisat')){
 if(!function_exists('IDateTimes')){
 // @codeCoverageIgnoreEnd
 
-    function IDateTimes($strDate='', $withTimes=true): string {
+	/**
+	 * Indonesian Date Times
+	 * @param $strDate		string	Date Times in string ex: yyyy-mm-dd h:i:s
+	 * @param $withTimes	bool	Include times in format H:i:s
+	 * 
+	 * @return string	DateTimes in string ex: Minggu, 01 Mei 2022 H:i:s
+	 */
+    function IDateTimes(string $strDate='', $withTimes=true): string {
 	    $date = strtotime(!empty($strDate)?$strDate:(new DateTimeImmutable("now",new DateTimeZone("Asia/Jakarta")))->format("Y-m-d H:i:s"));
 	    $monthList = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
 	    $dayList = ["Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"];
@@ -38,7 +50,14 @@ if(!function_exists('IDateTimes')){
 if(!function_exists('dateDiff')){
 // @codeCoverageIgnoreEnd
 
-    function dateDiff($dates=null,$now=null): string {
+	/**
+	 * Get interval between two dates
+	 * @param $dates	null|string	Date with format yyyy-mm-dd
+	 * @param $now		null|string Now or Custom Date
+	 * 
+	 * @return string	xx Tahun xx Bulan xx Hari
+	 */
+    function dateDiff(?string $dates=null, ?string $now=null): string {
 	    if(empty($dates) && empty($now)) return "";
 
 	    $date = new DateTimeImmutable($dates, new DateTimeZone("Asia/Jakarta"));
@@ -64,4 +83,220 @@ if(!function_exists('dateDiff')){
     }
 }
 
+// @codeCoverageIgnoreStart
+if(!function_exists('get_client_ip')){
+// @codeCoverageIgnoreEnd
 
+	/**
+	 * Get Client IP
+	 * 
+	 * @return string IP
+	 */
+	function getClientIp(): string {
+		$ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+	}
+}
+
+// @codeCoverageIgnoreStart
+if(!function_exists('uuid_v3')){
+// @codeCoverageIgnoreEnd
+
+    function uuid_v3($namespace, $name) {
+        if(!is_valid_uuid($namespace)) return false;
+    
+        // Get hexadecimal components of namespace
+        $nhex = str_replace(array('-','{','}'), '', $namespace);
+    
+        // Binary Value
+        $nstr = '';
+    
+        // Convert Namespace UUID to bits
+        for($i = 0; $i < strlen($nhex); $i+=2) {
+          $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+        }
+    
+        // Calculate hash value
+        $hash = md5($nstr . $name);
+    
+        return sprintf('%08s-%04s-%04x-%04x-%12s',
+            // 32 bits for "time_low"
+            substr($hash, 0, 8),
+
+            // 16 bits for "time_mid"
+            substr($hash, 8, 4),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 3
+            (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+          substr($hash, 20, 12)
+        );
+    }
+}
+
+// @codeCoverageIgnoreStart
+if(!function_exists('uuid_v4')){
+// @codeCoverageIgnoreEnd
+
+    function uuid_v4() {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+        
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+        
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+        
+            // 48 bits for "node"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+}
+
+// @codeCoverageIgnoreStart
+if(!function_exists('uuid_v5')){
+// @codeCoverageIgnoreEnd
+
+    function uuid_v5($namespace, $name) {
+        if(!is_valid_uuid($namespace)) return false;
+    
+        // Get hexadecimal components of namespace
+        $nhex = str_replace(array('-','{','}'), '', $namespace);
+    
+        // Binary Value
+        $nstr = '';
+    
+        // Convert Namespace UUID to bits
+        for($i = 0; $i < strlen($nhex); $i+=2) {
+          $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+        }
+    
+        // Calculate hash value
+        $hash = sha1($nstr . $name);
+    
+        return sprintf('%08s-%04s-%04x-%04x-%12s',
+            // 32 bits for "time_low"
+            substr($hash, 0, 8),
+        
+            // 16 bits for "time_mid"
+            substr($hash, 8, 4),
+        
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 5
+            (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+        
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+        
+            // 48 bits for "node"
+            substr($hash, 20, 12)
+        );
+    }
+}
+
+// @codeCoverageIgnoreStart
+if(!function_exists('is_valid_uuid')){
+// @codeCoverageIgnoreEnd
+
+    function is_valid_uuid($uuid) {
+        return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.'[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
+    }
+}
+
+// @codeCoverageIgnoreStart
+if(!function_exists('setMyHeader')){
+// @codeCoverageIgnoreEnd
+
+	function setMyHeader(?int $code = NULL, ?string $msg = NULL): ?object {
+		$obj = new StdClass();
+		if (!empty($code)){
+			switch ($code) {
+				case 100: $text = 'Continue'; break;
+				case 101: $text = 'Switching Protocols'; break;
+				case 200: $text = 'OK'; break;
+				case 201: $text = 'Created'; break;
+				case 202: $text = 'Accepted'; break;
+				case 203: $text = 'Non-Authoritative Information'; break;
+				case 204: $text = 'No Content'; break;
+				case 205: $text = 'Reset Content'; break;
+				case 206: $text = 'Partial Content'; break;
+				case 300: $text = 'Multiple Choices'; break;
+				case 301: $text = 'Moved Permanently'; break;
+				case 302: $text = 'Moved Temporarily'; break;
+				case 303: $text = 'See Other'; break;
+				case 304: $text = 'Not Modified'; break;
+				case 305: $text = 'Use Proxy'; break;
+				case 400: $text = 'Bad Request'; break;
+				case 401: $text = 'Unauthorized'; break;
+				case 402: $text = 'Payment Required'; break;
+				case 403: $text = 'Forbidden'; break;
+				case 404: $text = 'Not Found'; break;
+				case 405: $text = 'Method Not Allowed'; break;
+				case 406: $text = 'Not Acceptable'; break;
+				case 407: $text = 'Proxy Authentication Required'; break;
+				case 408: $text = 'Request Time-out'; break;
+				case 409: $text = 'Conflict'; break;
+				case 410: $text = 'Gone'; break;
+				case 411: $text = 'Length Required'; break;
+				case 412: $text = 'Precondition Failed'; break;
+				case 413: $text = 'Request Entity Too Large'; break;
+				case 414: $text = 'Request-URI Too Large'; break;
+				case 415: $text = 'Unsupported Media Type'; break;
+				case 500: $text = 'Internal Server Error'; break;
+				case 501: $text = 'Not Implemented'; break;
+				case 502: $text = 'Bad Gateway'; break;
+				case 503: $text = 'Service Unavailable'; break;
+				case 504: $text = 'Gateway Time-out'; break;
+				case 505: $text = 'HTTP Version not supported'; break;
+				case 600: $text = 'Database Unavailable'; break;
+				default:
+					// trigger_error('Unknown http status code "' . htmlentities(strval($code)) . '"',E_USER_ERROR);
+					return null;
+				break;
+			}
+
+			$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+			$obj->protocol = $protocol;
+			$obj->code = $code;
+			$obj->text = $text;
+			$obj->json = json_encode(array('code'=>$code,'status'=>(!empty($msg)?$msg:$text)));
+		}else{
+			$obj->protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+			$obj->code = 200;
+			$obj->text = 'OK';
+			$obj->json = json_encode(array('code'=>200,'status'=>(!empty($msg)?$msg:'OK')));
+		}
+		return $obj;
+	}
+}
--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -30,26 +30,160 @@ class HelperTest extends TestCase {
 
     public function testDateDiffWithoutParams(): void
     {
-	$this->assertEmpty(dateDiff());
+	    $this->assertEmpty(dateDiff());
     }
 
     public function testDateDiffWithParams(): void
     {
-	$this->assertEquals("31 Tahun 10 Bulan 27 Hari",dateDiff((new DateTimeImmutable("1990-06-04", new DateTimeZone("Asia/Jakarta")))->format("Y-m-d"),"2022-05-01"));
-	if(preg_match('/8.1/',phpversion())){
-		$this->assertEquals("01 Bulan",dateDiff("2022-04-01","2022-05-01"));
-		$this->assertEquals("01 Bulan 10 Hari",dateDiff("2022-04-01","2022-05-11"));
-		$this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
-		$this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
-		$this->assertEquals("01 Tahun 02 Bulan",dateDiff("2021-03-01","2022-05-01"));
-	}else{
-		$this->assertEquals("30 Hari",dateDiff("2022-04-01","2022-05-01"));
-		$this->assertEquals("01 Bulan 09 Hari",dateDiff("2022-04-01","2022-05-11"));
-		$this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
-		$this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
-		$this->assertEquals("01 Tahun 02 Bulan 02 Hari",dateDiff("2021-03-01","2022-05-01"));
+        $this->assertEquals("31 Tahun 10 Bulan 27 Hari",dateDiff((new DateTimeImmutable("1990-06-04", new DateTimeZone("Asia/Jakarta")))->format("Y-m-d"),"2022-05-01"));
+        if(preg_match('/8.1/',phpversion())){
+            $this->assertEquals("01 Bulan",dateDiff("2022-04-01","2022-05-01"));
+            $this->assertEquals("01 Bulan 10 Hari",dateDiff("2022-04-01","2022-05-11"));
+            $this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
+            $this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
+            $this->assertEquals("01 Tahun 02 Bulan",dateDiff("2021-03-01","2022-05-01"));
+        }else{
+            $this->assertEquals("30 Hari",dateDiff("2022-04-01","2022-05-01"));
+            $this->assertEquals("01 Bulan 09 Hari",dateDiff("2022-04-01","2022-05-11"));
+            $this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
+            $this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
+            $this->assertEquals("01 Tahun 02 Bulan 02 Hari",dateDiff("2021-03-01","2022-05-01"));
 
-	}
-	$this->assertEquals("0 Hari",dateDiff("2022-05-01","2022-05-01"));
+        }
+        $this->assertEquals("0 Hari",dateDiff("2022-05-01","2022-05-01"));
+    }
+
+    public function testGetClientIp(): void
+    {
+        $this->assertEquals("UNKNOWN",getClientIp());
+        putenv('HTTP_CLIENT_IP=127.0.0.1');
+        $this->assertEquals("127.0.0.1",getClientIp());
+        putenv('HTTP_CLIENT_IP');
+        putenv('HTTP_X_FORWARDED_FOR=127.0.0.1');
+        $this->assertEquals("127.0.0.1",getClientIp());
+        putenv('HTTP_X_FORWARDED_FOR');
+        putenv('HTTP_X_FORWARDED=127.0.0.1');
+        $this->assertEquals("127.0.0.1",getClientIp());
+        putenv('HTTP_X_FORWARDED');
+        putenv('HTTP_FORWARDED_FOR=127.0.0.1');
+        $this->assertEquals("127.0.0.1",getClientIp());
+        putenv('HTTP_FORWARDED_FOR');
+        putenv('HTTP_FORWARDED=127.0.0.1');
+        $this->assertEquals("127.0.0.1",getClientIp());
+        putenv('HTTP_FORWARDED');
+        putenv('REMOTE_ADDR=127.0.0.1');
+        $this->assertEquals("127.0.0.1",getClientIp());
+    }
+
+    public function testUuid_v3(): void
+    {
+        $this->assertEquals("eb83e28d-ab7e-3ad7-8d7e-8caeb9eedc37",uuid_v3("a7950197-cac7-4cd5-b29f-31c45bc1341f","ekojunaidisalam.com"));
+    }
+
+    public function testUuid_v4(): void
+    {
+        $this->assertTrue(is_valid_uuid(uuid_v4()));
+    }
+
+    public function testUuid_v5(): void
+    {
+        $this->assertEquals("0459d018-0a8e-5d72-89be-dffcd96c7dad",uuid_v5("a7950197-cac7-4cd5-b29f-31c45bc1341f","ekojunaidisalam.com"));
+    }
+
+    public function testSetMyHeaderNull(): void
+    {
+        $this->assertEquals(200,setMyHeader()->code);
+        $this->assertEquals("OK",setMyHeader()->text);
+        $this->assertEquals('{"code":200,"status":"OK"}',setMyHeader()->json);
+    }
+
+    public function testSetMyHeaderInvalid(): void
+    {
+        // $this->expectExceptionMessageMatches("/Unknown http status code/");
+        $this->assertNull(setMyHeader(99));
+    }
+
+    public function testSetMyHeaderCode(): void
+    {
+        $this->assertEquals(100,setMyHeader(100)->code);
+        $this->assertEquals(101,setMyHeader(101)->code);
+        $this->assertEquals(200,setMyHeader(200)->code);
+        $this->assertEquals(201,setMyHeader(201)->code);
+        $this->assertEquals(202,setMyHeader(202)->code);
+        $this->assertEquals(203,setMyHeader(203)->code);
+        $this->assertEquals(204,setMyHeader(204)->code);
+        $this->assertEquals(205,setMyHeader(205)->code);
+        $this->assertEquals(206,setMyHeader(206)->code);
+        $this->assertEquals(300,setMyHeader(300)->code);
+        $this->assertEquals(301,setMyHeader(301)->code);
+        $this->assertEquals(302,setMyHeader(302)->code);
+        $this->assertEquals(303,setMyHeader(303)->code);
+        $this->assertEquals(304,setMyHeader(304)->code);
+        $this->assertEquals(305,setMyHeader(305)->code);
+        $this->assertEquals(400,setMyHeader(400)->code);
+        $this->assertEquals(401,setMyHeader(401)->code);
+        $this->assertEquals(402,setMyHeader(402)->code);
+        $this->assertEquals(403,setMyHeader(403)->code);
+        $this->assertEquals(404,setMyHeader(404)->code);
+        $this->assertEquals(405,setMyHeader(405)->code);
+        $this->assertEquals(406,setMyHeader(406)->code);
+        $this->assertEquals(407,setMyHeader(407)->code);
+        $this->assertEquals(408,setMyHeader(408)->code);
+        $this->assertEquals(409,setMyHeader(409)->code);
+        $this->assertEquals(410,setMyHeader(410)->code);
+        $this->assertEquals(411,setMyHeader(411)->code);
+        $this->assertEquals(412,setMyHeader(412)->code);
+        $this->assertEquals(413,setMyHeader(413)->code);
+        $this->assertEquals(414,setMyHeader(414)->code);
+        $this->assertEquals(415,setMyHeader(415)->code);
+        $this->assertEquals(500,setMyHeader(500)->code);
+        $this->assertEquals(501,setMyHeader(501)->code);
+        $this->assertEquals(502,setMyHeader(502)->code);
+        $this->assertEquals(503,setMyHeader(503)->code);
+        $this->assertEquals(504,setMyHeader(504)->code);
+        $this->assertEquals(505,setMyHeader(505)->code);
+        $this->assertEquals(600,setMyHeader(600)->code);
+    }
+
+    public function testSetMyHeaderText(): void
+    {
+        $this->assertEquals("Continue",setMyHeader(100)->text);
+        $this->assertEquals("Switching Protocols",setMyHeader(101)->text);
+        $this->assertEquals("OK",setMyHeader(200)->text);
+        $this->assertEquals("Created",setMyHeader(201)->text);
+        $this->assertEquals("Accepted",setMyHeader(202)->text);
+        $this->assertEquals("Non-Authoritative Information",setMyHeader(203)->text);
+        $this->assertEquals("No Content",setMyHeader(204)->text);
+        $this->assertEquals("Reset Content",setMyHeader(205)->text);
+        $this->assertEquals("Partial Content",setMyHeader(206)->text);
+        $this->assertEquals("Multiple Choices",setMyHeader(300)->text);
+        $this->assertEquals("Moved Permanently",setMyHeader(301)->text);
+        $this->assertEquals("Moved Temporarily",setMyHeader(302)->text);
+        $this->assertEquals("See Other",setMyHeader(303)->text);
+        $this->assertEquals("Not Modified",setMyHeader(304)->text);
+        $this->assertEquals("Use Proxy",setMyHeader(305)->text);
+        $this->assertEquals("Bad Request",setMyHeader(400)->text);
+        $this->assertEquals("Unauthorized",setMyHeader(401)->text);
+        $this->assertEquals("Payment Required",setMyHeader(402)->text);
+        $this->assertEquals("Forbidden",setMyHeader(403)->text);
+        $this->assertEquals("Not Found",setMyHeader(404)->text);
+        $this->assertEquals("Method Not Allowed",setMyHeader(405)->text);
+        $this->assertEquals("Not Acceptable",setMyHeader(406)->text);
+        $this->assertEquals("Proxy Authentication Required",setMyHeader(407)->text);
+        $this->assertEquals("Request Time-out",setMyHeader(408)->text);
+        $this->assertEquals("Conflict",setMyHeader(409)->text);
+        $this->assertEquals("Gone",setMyHeader(410)->text);
+        $this->assertEquals("Length Required",setMyHeader(411)->text);
+        $this->assertEquals("Precondition Failed",setMyHeader(412)->text);
+        $this->assertEquals("Request Entity Too Large",setMyHeader(413)->text);
+        $this->assertEquals("Request-URI Too Large",setMyHeader(414)->text);
+        $this->assertEquals("Unsupported Media Type",setMyHeader(415)->text);
+        $this->assertEquals("Internal Server Error",setMyHeader(500)->text);
+        $this->assertEquals("Not Implemented",setMyHeader(501)->text);
+        $this->assertEquals("Bad Gateway",setMyHeader(502)->text);
+        $this->assertEquals("Service Unavailable",setMyHeader(503)->text);
+        $this->assertEquals("Gateway Time-out",setMyHeader(504)->text);
+        $this->assertEquals("HTTP Version not supported",setMyHeader(505)->text);
+        $this->assertEquals("Database Unavailable",setMyHeader(600)->text);
     }
 }
