8cfcc1da4e5b81b35e0021dd3b16d6853a74d6c5
Eko Junaidi Salam <eko.junaidi.salam@gmail.com>
2022-05-01 15:16:37 +0700

    Add Indonesian Date Times, and Indonesian Date Diff

--- src/Helper.php
+++ src/Helper.php
@@ -10,8 +10,58 @@
 if(!function_exists('is_kabisat')){
 // @codeCoverageIgnoreEnd
 
-    function is_kabisat($tahun){
-        $tahun = intval($tahun);
+    function is_kabisat($tahun=null): int {
+        $tahun = intval(!empty($tahun) ? $tahun : date('Y'));
         return ((($tahun % 4 == 0) && ($tahun % 100 != 0) || ($tahun % 400 == 0))?1:0);
     }
 }
+
+// @codeCoverageIgnoreStart
+if(!function_exists('IDateTimes')){
+// @codeCoverageIgnoreEnd
+
+    function IDateTimes($strDate='', $withTimes=true): string {
+	    $date = strtotime(!empty($strDate)?$strDate:(new DateTimeImmutable("now",new DateTimeZone("Asia/Jakarta")))->format("Y-m-d H:i:s"));
+	    $monthList = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+	    $dayList = ["Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"];
+
+	    $dayName = $dayList[date('w',$date)];
+	    $day = date('d',$date);
+	    $month = $monthList[date('n',$date)-1];
+	    $year = date('Y',$date);
+	    $times = $withTimes ? " ".date('H:i:s', $date) : null;
+	    return "$dayName, $day $month $year".$times;
+    }
+}
+
+// @codeCoverageIgnoreStart
+if(!function_exists('dateDiff')){
+// @codeCoverageIgnoreEnd
+
+    function dateDiff($dates=null,$now=null): string {
+	    if(empty($dates) && empty($now)) return "";
+
+	    $date = new DateTimeImmutable($dates, new DateTimeZone("Asia/Jakarta"));
+	    $now = new DateTimeImmutable(!empty($now) ? $now : "now", new DateTimeZone("Asia/Jakarta"));
+	    $interval = $date->diff($now);
+
+	    $sameDay = intval($interval->format("%a"));
+	    if($sameDay > 0){
+		    [$year,$month,$day] = explode(" ",$interval->format("%Y %M %D"));
+		    $years = intval($year) ?? null;
+		    $months = intval($month) ?? null;
+		    $days = intval($day) ?? null;
+	    }else{
+		    $days = 1;
+		    $day = $sameDay;
+	    }
+
+	    $str = "";
+	    if(!empty($years)) $str .= $year." Tahun";
+	    if(!empty($months)) $str .= (!empty($years) ? " ":null).$month." Bulan";
+	    if(!empty($days)) $str .= (!empty($months) || !empty($years)? " ":null).$day." Hari";
+	    return $str;
+    }
+}
+
+
--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -3,10 +3,44 @@
 namespace EJSHelper\Tests;
 
 use PHPUnit\Framework\TestCase;
+use DateTimeImmutable;
+use DateTimeZone;
 
 class HelperTest extends TestCase {
+
     public function testIsKabisat(): void
     {
         $this->assertEquals(1,is_kabisat(2020));
+    }
+
+    public function testIDateTimesWithTimes(): void
+    {
+        $this->assertEquals("Minggu, 01 Mei 2022 10:34:12", IDateTimes("2022-05-01 10:34:12"));
+    }
+
+    public function testIDateTimesNoTimes(): void
+    {
+        $this->assertEquals("Minggu, 01 Mei 2022", IDateTimes((new DateTimeImmutable("2022-05-01", new DateTimeZone("Asia/Jakarta")))->format("Y-m-d"),false));
+    }
+
+    public function testIDateTimesWithoutParams(): void
+    {
+        $this->assertIsString(IDateTimes());
+    }
+
+    public function testDateDiffWithoutParams(): void
+    {
+	$this->assertEmpty(dateDiff());
+    }
+
+    public function testDateDiffWithParams(): void
+    {
+	$this->assertEquals("31 Tahun 10 Bulan 27 Hari",dateDiff((new DateTimeImmutable("1990-06-04", new DateTimeZone("Asia/Jakarta")))->format("Y-m-d"),"2022-05-01"));
+	$this->assertEquals("01 Bulan",dateDiff("2022-04-01","2022-05-01"));
+	$this->assertEquals("01 Bulan 10 Hari",dateDiff("2022-04-01","2022-05-11"));
+	$this->assertEquals("01 Tahun 02 Bulan",dateDiff("2021-03-01","2022-05-01"));
+	$this->assertEquals("01 Tahun 02 Hari",dateDiff("2021-05-01","2022-05-03"));
+	$this->assertEquals("12 Hari",dateDiff("2022-05-01","2022-05-13"));
+	$this->assertEquals("0 Hari",dateDiff("2022-05-01","2022-05-01"));
     }
 }
